240e6bc217a8603b3aedabe194d754a2a9d48a5d
Angel Mares <chuisangel8@gmail.com>
2026-07-26 14:39:20 -0600

    Update event data

--- src/data/eventConfig.js
+++ src/data/eventConfig.js
@@ -2,9 +2,9 @@ var name = "Anel";
 export const eventConfig = {
   title: `Baby Shower de ${name}`,
   name: name,
-  subtitle: "Acompáñanos a celebrar la dulce espera con juegos, brunch y sorpresas.",
+  subtitle: "Acompáñanos a celebrar la dulce espera con juegos, brunch y sorpresas. Recuerda confirmar tu asistencia",
   date: "2026-08-01T12:00:00-06:00",
-  place: "Texcoco",
+  place: "Texcoco, a 5 min de la Feria del Caballo",
   giftTableUrl: "https://mesaderegalos.liverpool.com.mx/milistaderegalos/51997221",
   giftTableLabel: "Ver mesa de regalos",
   timeline: [
